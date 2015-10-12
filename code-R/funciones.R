bb1e6c0d9f13bd9a727bd68e6754a1f0fe152350
Andriy-T <atkach.ua@gmail.com>
2015-10-12 13:35:03 +0200

    nuevas funciones
    del fichero progTool.R
    Además he añadido "func." a cada función nuestra para diferenciar dentro
    de los objetos en workspace

--- code-R/funciones.R
+++ code-R/funciones.R
@@ -10,3 +10,39 @@ func.factor.format <- function(data){
     } else stop("Input tiene que ser data.frame o data.table")
     
 }
+
+##### Manejo de objetos R #####
+
+CTE_ToName_sep  <- "." # el separador por defecto de R es "." 
+# lista con las subsituciones a efecturar para eliminar caracters raros
+CTE_ToName  <- list(
+    c("?","a"),c("?","e"),c("?","i"),c("?","o"),c("?","u")
+    ,c("?","A"),c("?","E"),c("?","I"),c("?","O"),c("?","U")
+    ,c(" ",CTE_ToName_sep),c("/",CTE_ToName_sep)
+    ,c("[",CTE_ToName_sep),c("]",CTE_ToName_sep),c("(",CTE_ToName_sep),c(")",CTE_ToName_sep)
+)
+
+func.mygsub <- function(x, txt){
+    gsub(txt[1],txt[2],x,fixed =TRUE)
+}
+
+# NOTA: esta funci?n es de manejo de strings habria que cambiarla de sitio
+func.ElimDup <- function(txt, dup){
+    dup2 <- paste(dup,dup, sep ="")  
+    while (grepl(dup2,txt,fixed = TRUE))  {txt  <- gsub(dup2, dup,txt,fixed =TRUE)}
+    ElimDup <- txt
+}
+
+#efectua las subsituciones marcadas por 'CTE_ToName'
+func.ToName <- function (nom){
+    nom2 <- Reduce(mygsub,CTE_ToName,init = nom)
+    ToNames <- apply(as.matrix(nom2), 1, ElimDup, dup = CTE_ToName_sep)
+}
+
+##### Existen un objeto #####
+
+# ¡¡¡ Ya existe una funcion: base::exists()
+
+# Exist <- function (nom){
+#     ( nom %in% ls(pattern = nom))
+# }
