973836d013d733e354ecc4081ad6850f2c13c354
asalafranca <asalafranca@gmail.com>
2015-10-14 19:46:56 +0200

    Directorios diferenciados por usuario

--- code-R/ini.R
+++ code-R/ini.R
@@ -1,11 +1,15 @@
 # fichero a ejecutar al iniciarla sesión
 
 # DirCode <- "C:/Users/Alfonso/Documents/GitHub/matriculaciones/code-R"  # ¡¡¡ cambiar
-DirCode <- "C:/Users/Andrey/Documents/GitHub/matriculaciones/code-R"
+if (Sys.info()["user"] == "Alfonso") DirCode <- "C:/Alfonso/cosas/software/R/matriculaciones/code-R" 
+  else  DirCode <- "C:/Users/Andrey/Documents/GitHub/matriculaciones/code-R" 
 
 #### directorio Proyecto y librerias ####
 # DirPro  <- "C:/Users/Alfonso/Dropbox/proyectos/dgt"
-DirPro  <- "C:/Users/Andrey/Dropbox/dgt"
+
+if (Sys.info()["user"] == "Alfonso") DirPro  <- "C:/Users/Alfonso/Dropbox/proyectos/dgt"
+else  DirPro  <- "C:/Users/Andrey/Dropbox/dgt"
+
 
 CargaIniPro <- TRUE # si CargaIniPro es TRUE cargamos las funciones y datos
                     # en otro caso solamente se cargan las librerías
@@ -16,3 +20,7 @@ if(!CargaIniPro) source(file.path(DirCode, "librerias.R"))
 if(CargaIniPro) source(file.path(DirCode, "funciones.R"))
 # cargar data
 if(CargaIniPro) source(file.path(DirCode, "data.R"))
+
+
+
+
