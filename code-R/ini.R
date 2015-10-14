7da9eb371d258a1308547fe28b2d7d5d9383c081
Andriy-T <atkach.ua@gmail.com>
2015-10-14 19:44:37 +0200

    if statment para los directorios
    Asignación de directorios en función del usuario

--- code-R/ini.R
+++ code-R/ini.R
@@ -1,14 +1,16 @@
-# fichero a ejecutar al iniciarla sesión
+# fichero a ejecutar al iniciarla sesi?n
 
-# DirCode <- "C:/Users/Alfonso/Documents/GitHub/matriculaciones/code-R"  # ¡¡¡ cambiar
-DirCode <- "C:/Users/Andrey/Documents/GitHub/matriculaciones/code-R"
+if(Sys.info()[7] == "Alfonso")
+    DirCode <- "C:/Users/Alfonso/Documents/GitHub/matriculaciones/code-R"
+if(Sys.info()[7] == "Andrey")
+    DirCode <- "C:/Users/Andrey/Documents/GitHub/matriculaciones/code-R"
 
 #### directorio Proyecto y librerias ####
 # DirPro  <- "C:/Users/Alfonso/Dropbox/proyectos/dgt"
 DirPro  <- "C:/Users/Andrey/Dropbox/dgt"
 
 CargaIniPro <- TRUE # si CargaIniPro es TRUE cargamos las funciones y datos
-                    # en otro caso solamente se cargan las librerías
+                    # en otro caso solamente se cargan las librer?as
 
 # cargar librerias
 if(!CargaIniPro) source(file.path(DirCode, "librerias.R"))
