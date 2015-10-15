1c3563baf07196a0bbb14fe1c2294e85595baf20
asalafranca <asalafranca@gmail.com>
2015-10-15 10:11:20 +0200

    Directorios diferenciados por usuario

--- code-R/ini.R
+++ code-R/ini.R
@@ -1,20 +1,22 @@
-# fichero a ejecutar al iniciarla sesi?n
+##################################################################
+# fichero a ejecutar al iniciarla sesion
+##################################################################
 
-if(Sys.info()[7] == "Alfonso")
-    DirCode <- "C:/Users/Alfonso/Documents/GitHub/matriculaciones/code-R"
-if(Sys.info()[7] == "Andrey")
-    DirCode <- "C:/Users/Andrey/Documents/GitHub/matriculaciones/code-R"
+
+if (Sys.info()["user"] == "Alfonso") DirCode <- "C:/Alfonso/cosas/software/R/matriculaciones/code-R" 
+else  DirCode <- "C:/Users/Andrey/Documents/GitHub/matriculaciones/code-R" 
+DirCode <- "C:/Users/Andrey/Documents/GitHub/matriculaciones/code-R"
 
 #### directorio Proyecto y librerias ####
 # DirPro  <- "C:/Users/Alfonso/Dropbox/proyectos/dgt"
-DirPro  <- "C:/Users/Andrey/Dropbox/dgt"
+
+if (Sys.info()["user"] == "Alfonso") DirPro  <- "C:/Users/Alfonso/Dropbox/proyectos/dgt"
+else  DirPro  <- "C:/Users/Andrey/Dropbox/dgt"
 
 CargaIniPro <- TRUE # si CargaIniPro es TRUE cargamos las funciones y datos
-                    # en otro caso solamente se cargan las librer?as
+# en otro caso solamente se cargan las librerias
 
-# cargar librerias
 if(!CargaIniPro) source(file.path(DirCode, "librerias.R"))
-# cargar funciones
 if(CargaIniPro) source(file.path(DirCode, "funciones.R"))
 # cargar data
 if(CargaIniPro) source(file.path(DirCode, "data.R"))
