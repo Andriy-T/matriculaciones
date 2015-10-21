54c2ff5497a104415fc9a126ecff8e9b6a698aa2
Andriy-T <atkach.ua@gmail.com>
2015-10-21 23:52:49 +0200

    Graf. TS dia
    Formateos varios

--- code-R/graficos.R
+++ code-R/graficos.R
@@ -1,46 +1,51 @@
-# Gráfico temporal de linea
-# Gráfico
+########################################################+
+# Gráfico temporal de matriculaciones
+########################################################+
 
-tmp_df <- toJSONArray2(matric_tot_dia, json = F, name = F)
-tmp_df <- toJSONArray(matric_tot_dia, json = F)
-tmp_df <- matric_tot_dia
+# transformamos la tabla para visualizar bien las fechas
+tmp_df_2 <- transform(matric_tot_dia
+                      , Date = as.numeric(as.POSIXct(fecha))*1000)
 
-# libreria jsonlite
-tmp_df_json <- toJSON(matric_tot_dia)
+# -------------------------------------------------------+
+# Grafico -----
+# -------------------------------------------------------+
 
-# con libreria rCharts
-tmp_df_rch <- make_dataset("fecha", "matric", tmp_df)
-
-tmp_df_2 <- matric_tot_dia
-tmp_df_2 <- transform(tmp_df_2, Date = as.numeric(as.POSIXct(fecha))*1000)
-
-h1 <- hPlot(matric ~ Date, data = tmp_df_2, 
-               # group = 'Type', 
+# Definicion del grafico y los datos
+graf.ts_matric <- hPlot(matric ~ Date, data = tmp_df_2, 
                type = "area"
-               # radius=5
 )
-h1$xAxis(type = 'datetime', labels = list(
-    format = '{value:%d/%m/%Y}'  
-))
-h1$chart(zoomType = "x")
-h1
+# Titulo
+graf.ts_matric$title(text = 'Matricualciones diarias')
 
-graf.ts_matric <- Highcharts$new()
-# graf.ts_matric$chart(type = "area")
-graf.ts_matric$xAxis(type = 'datetime', labels = list(
-    format = '{value:%d/%m/%Y}'  
-))
+# Opciones del grafico
 graf.ts_matric$chart(zoomType = "x")
+graf.ts_matric$params$width ="100%"
+# graf.ts_matric$exporting(enabled=T)
 
-graf.ts_matric$series(name ="matric", type = "area",
-                      data = 
-                          tmp_df_2[, matric]
-#                           toJSONArray(tmp_df_2[, .(Date, matric)]
-#                                          , json = F)
-                      , yAxis = 0)
-# graf.ts_matric$plotOptions(type="area")
+graf.ts_matric$plotOptions(
+    area = list(marker = list(enabled = F), stacking = "normal"
+                  , fillOpacity = .3, lineWidth = 1
+                , allowPointSelect = T, color = 'rgba(67,162,202,1)')
+)
 
-graf.ts_matric
+graf.ts_matric$tooltip(shared = T, crosshairs = list(T, T))
+# graf.ts_matric$colors()
+
+# Ejes
+graf.ts_matric$xAxis(type = 'datetime'
+         # , labels = list(format = '{value:%d/%m/%Y}')
+         )
+
+graf.ts_matric$yAxis(
+    list(list(title = list(text = ''), min = 0, opposite = F
+              , gridLineWidth = .8, gridLineColor = 'rgba(242,242,242,1)'
+              , startOnTick = F, endOnTick = F
+                  # , gridLineDashStyle = "Dot"
+                  )
+)
+)
 
 # devolver codigo html del objeto
 graf.ts_matric$show("inline", include_assets = FALSE)
+
+graf.ts_matric
