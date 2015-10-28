4d31a239c788be670d7b5e6ddb7e7a0ef9732b92
asalafranca <asalafranca@gmail.com>
2015-10-28 18:44:29 +0100

    Detalles menores
    Parametrizado DirPro segunusuario
    Las librerias se cargan siempre
    Eliminadas las tildes de los comentarios

--- code-R/ini.R
+++ code-R/ini.R
@@ -1,16 +1,17 @@
-# fichero a ejecutar al iniciarla sesi?n
+##################################################################
+# fichero a ejecutar al iniciarla sesion
+##################################################################
 
-if(Sys.info()[7] == "Alfonso")
-    DirCode <- "C:/Users/Alfonso/Documents/GitHub/matriculaciones/code-R"
-if(Sys.info()[7] == "Andrey")
-    DirCode <- "C:/Users/Andrey/Documents/GitHub/matriculaciones/code-R"
+if (Sys.info()["user"] == "Alfonso") DirCode <- "C:/Alfonso/cosas/software/R/matriculaciones/code-R" else
+  DirCode <- "C:/Users/Andrey/Documents/GitHub/matriculaciones/code-R" 
 
 #### directorio Proyecto y librerias ####
-# DirPro  <- "C:/Users/Alfonso/Dropbox/proyectos/dgt"
-DirPro  <- "C:/Users/Andrey/Dropbox/dgt"
+if (Sys.info()["user"] == "Alfonso") DirPro  <- "C:/Users/Alfonso/Dropbox/proyectos/dgt" else
+  DirPro  <- "C:/Users/Andrey/Dropbox/dgt"
 
-CargaIniPro <- T # si CargaIniPro es TRUE cargamos las funciones y datos
-                    # en otro caso solamente se cargan las librer?as
+
+CargaIniPro <- T # si CargaIniPro es TRUE cargamos las librerias, funciones y datos
+                    # en otro caso solamente se cargan las librerias
 
 # cargar librerias
 source(file.path(DirCode, "librerias.R"))
