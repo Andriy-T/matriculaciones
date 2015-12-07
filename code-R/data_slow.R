db95352d4afa7dfa4d8a3190b25d015f5043f743
asalafranca <asalafranca@gmail.com>
2015-12-07 23:45:08 +0100

    Nueva version del modelo, se almacena en modeloCorto

--- code-R/data_slow.R
+++ code-R/data_slow.R
@@ -29,8 +29,10 @@ col_sel <- c("FEC_MATRICULA", "MARCA_ITV", "MODELO_ITV"
              , "CATEGORIA_HOMOLOGACION_EUROPEA_ITV", "NIVEL_EMISIONES_EURO_ITV"
              , "CONSUMO.WH.KM_ITV", "CLASIFICACION_REGLAMENTO_VEHICULOS_ITV"
              , "MUNICIPIO", "COD_PROVINCIA_VEH"
-             , "modelo1"
+             , "SERVICIO"
+             , "modeloCorto"
 )
+
 colFech <- c("FEC_MATRICULA","FEC_TRAMITACION","FEC_TRAMITE","FEC_PRIM_MATRICULACION","FEC_PROCESO")
 
 
@@ -67,7 +69,7 @@ data_all <- data_all[COD_TIPO == "40" | COD_TIPO == "25"]
 if (numReg != nrow(data_all) ){
   warning(cat("Eliminados", numReg - nrow(data_all) , "registros ("
               ,100*round((numReg - nrow(data_all)) /numReg,4)
-              ,"%) debido a tramite no matriculacion nueva\n"))        
+              ,"%) debido a tramite vehiculo no turismo o todoterreno\n"))        
 }
 
 # 4 IND_NUEVO_USADO == "N" Solo vehiculos nuevos
@@ -136,32 +138,21 @@ gc()
 # Para que las marcas de todos los ficheros tengan la misma longitud
 data_set[, MARCA_ITV := substr(as.character(MARCA_ITV),1,23)] 
 
-# eliminaos espacios del modelo
+# eliminamos espacios del modelo al principio y al final
 data_set[, MODELO_ITV := as.character(gsub("^\\s+|\\s+$", "",MODELO_ITV))]
 
 # marca 1 = primera palabra de la marca 
 f1 <- function(x){x[1]}
+f2 <- function(x){if (length(x)>1) x[2] else x[1]}
 data_set$marca1 <-   sapply(data_set$MARCA_ITV, strsplit, " ", simplify = T,fixed= T)
 data_set$marca1 <-   sapply(data_set$marca1, f1, simplify = T)
 
-
-# modelo 1 = primera palabra de la marca 
-data_set$modelo1 <- sapply(as.character(data_set$MODELO_ITV), strsplit, " ", simplify = T,fixed= T)
-data_set$modelo1 <- sapply(data_set$modelo1, f1, simplify = T)
-
-
 # marca 2 = marca sin espacios 
 data_set[, marca2 := as.character(MARCA_ITV)]
-data_set[, marca2 := gsub("^\\s+|\\s+$", "",marca2)]
+data_set[, marca2 := gsub("^\\s+|\\s+$", "", marca2)]
 #eliminamos del MODELO_ITV la marca
 data_set[, MODELO_ITV :=  mapply(gsub, marca2, "", MODELO_ITV, fixed= T)]
 data_set[, MODELO_ITV :=  mapply(gsub, marca1, "", MODELO_ITV, fixed= T)]
-
-# eliminaos carcateres raros de MODELO_ITV
-data_set[, MODELO_ITV := as.character(gsub("^\\s+|\\s+$", "",MODELO_ITV))]
-data_set[, modelo2 := func.ToName(MODELO_ITV)]
-data_set[, modelo2 :=  gsub(CTE_ToName_sep, "", modelo2, fixed = T)]
-
 
 # eliminaos espacios y carcateres raros de MARCA
 data_set[, marca2 := as.character(MARCA_ITV)]
@@ -170,12 +161,163 @@ data_set[, marca2 :=  gsub(CTE_ToName_sep, "", marca2, fixed = T)]
 
 data_set[, codItv2 :=  substr(as.character(CODIGO_ITV),1,8)]
 
+# eliminaMos espacios al final y al principio de MODELO_ITV
+data_set[, MODELO_ITV := gsub("^\\s+|\\s+$", "", as.character(MODELO_ITV))]
 
 data_set2 <- copy(data_set)
+
+setkeyv(data_set2,c("marca2") )
+
+
+#### Modificaciones en MARCA_ITV y MODELO_ITV 
+#data_set2[ ,modelo0 := modelo1]
+#prefijos a separar
+preMod <- c("SERIE", "", "AMG", "AMG", "GRAND ","GRAN", "GR ","GRAN", "GRAN","GRAN")
+dim(preMod) <- c(2,length(preMod)/2) 
+#sufijos a separar
+sufMod <- c("CONNECT","COURIER","CUSTOM","CABRIO","SPORTSVAN","MAX")
+# data_set2[,pre:= lapply(gregexpr("[0-9]",MODELO_ITV), getthepos)]
+
+# Separamos los prefijos
+data_set2[, MODELO_ITV2 :=MODELO_ITV]
+for (j in 1:dim(preMod)[2]){
+  data_set2[ grepl(paste("^",preMod[1,j],sep =""), MODELO_ITV2), pre := preMod[2,j] ]
+  data_set2[ grepl(paste("^",preMod[1,j],sep =""), MODELO_ITV2), MODELO_ITV2 := sub(preMod[1,j], "",MODELO_ITV2)]
+}
+data_set2[!is.na(pre), MODELO_ITV2 := gsub("^\\s+", "", MODELO_ITV2)]
+
+
+
+# Separamos los sufijos
+for (c in sufMod){
+  data_set2[ grepl(paste(c,sep =""), MODELO_ITV2), suf := c ]
+  data_set2[ grepl(paste(c,sep =""), MODELO_ITV2),  MODELO_ITV2 :=substr(MODELO_ITV2, 1, regexpr(c,MODELO_ITV2)-1)]
+  data_set2[!is.na(suf), MODELO_ITV2 := gsub("\\s+$", "", MODELO_ITV2)]
+}
+
+# modelo 1 = primera palabra del modelo
+# modelo 2 = segunda palabra del modelo
+data_set2$modelo1 <- sapply(data_set2$MODELO_ITV2, strsplit, " ", simplify = T,fixed= T)
+data_set2$modelo2 <- sapply(data_set2$modelo1, f2, simplify = T)
+data_set2$modelo1 <- sapply(data_set2$modelo1, f1, simplify = T)
+# modeloNoEsp es MODELO_ITV sin espacios ni carcateres raros 
+data_set2[, modeloNoEsp := func.ToName(MODELO_ITV2)]
+data_set2[, modeloNoEsp :=  gsub(CTE_ToName_sep, "", modeloNoEsp, fixed = T)]
+
+
+# si la primera palabra de modelo tiene más de cuatro caracteres nos la quedamos en MODELO_ITV2 y eliminamos el resto 
+data_set2[nchar(modelo1)>3, MODELO_ITV2:= modelo1]
+
+# modeloCorto modelo hasta que acaba el primer número por la izquierda
+# Ej AUDI A100d -> AUDI A100
+getthepos <- function (x)
+{
+  max(x[x==x[1]+c(1:length(x))-1])
+}
+data_set2[,modeloCorto:= MODELO_ITV2]
+data_set2[,aux:= lapply(gregexpr("[0-9]",MODELO_ITV2), getthepos)]
+data_set2[aux!=-1, modeloCorto:=substr(MODELO_ITV2,1,aux)]
+data_set2[aux!=-1, modeloCorto:=sub(" ","",modeloCorto)]
+data_set2[aux!=-1, modeloCorto:=sub("-","",modeloCorto)]
+
+
+# Cambiar 'JAGUARLANDROVERLIMIT'por 'JAGUAR'
+data_set2[marca2 == "JAGUARLANDROVERLIMIT", MARCA_ITV:= "LAND ROVER"]
+data_set2[marca2 == "JAGUARLANDROVERLIMIT", marca1:= "LAND ROVER"]
+data_set2[marca2 == "JAGUARLANDROVERLIMIT", marca2:= "LANDROVER"]
+
+# Cambiar 'MERCEDESBENZ' y MERCEDESAMG por 'MERCEDES
+# data_set2[marca2 == "MERCEDESAMG" & is.na(pre), pre:= "AMG"]
+data_set2[marca2 == "MERCEDESAMG", MARCA_ITV:= "MERCEDES"]
+data_set2[marca2 == "MERCEDESAMG", marca1:= "MERCEDES"]
+data_set2[marca2 == "MERCEDESAMG", marca2:= "MERCEDES"]
+data_set2[marca2 == "MERCEDESBENZ", MARCA_ITV:= "MERCEDES"]
+data_set2[marca2 == "MERCEDESBENZ", marca1:= "MERCEDES"]
+data_set2[marca2 == "MERCEDESBENZ", marca2:= "MERCEDES"]
+
+# Cambiar 'BMWI' por 'BMWI'
+data_set2[marca2 == "BMWI", MARCA_ITV:= "BMW"]
+data_set2[marca2 == "BMWI", marca1:= "BMW"] 
+data_set2[marca2 == "BMWI", marca2:= "BMW"]
+#En BMW , si el modelo empieza por num lo pasamos a serie más num	BMW
+data_set2[marca2 == "BMW" & modeloNoEsp <"9Z" & modeloNoEsp >"1", modeloCorto:= paste("SERIE ", substr(modeloNoEsp,1,1),sep="")]
+## En BMW , si el modelo empieza por "Serie" lo pasamos a serie más 2 caracteres BMW
+# data_set2[marca2 == "BMW" & modeloNoEsp =="SERIE", modeloCorto:= paste("SERIE ", substr(modeloNoEsp,6,6),sep="")]
+#En BMW , quitamos el espacio si modelo empieza por X' '*
+data_set2[marca2 == "BMW" & substr(modeloCorto,1,2) =="X ", modeloCorto:= paste("X", substr(modeloCorto,3,nchar(modeloCorto)) ,sep="")]
+          
+#en PEUGEOT si modelo empieza por N la quitamos
+data_set2[marca2 == "PEUGEOT" & substr(modeloNoEsp,1,1) =="N", modeloNoEsp:= substr(modeloNoEsp,2,nchar(modeloNoEsp))]
+data_set2[marca2 == "PEUGEOT" & substr(modelo1,1,1) =="N", modelo1:= substr(modelo1,2,nchar(modelo1))]
+data_set2[marca2 == "PEUGEOT" & substr(modeloCorto,1,1) =="N", modeloCorto:= substr(modeloCorto,2,nchar(modeloCorto))]
+data_set2[marca2 == "PEUGEOT" & substr(MODELO_ITV2,1,1) =="N", MODELO_ITV2:= substr(MODELO_ITV2,2,nchar(MODELO_ITV2))]
+
+##en PEUGEOT si empieza por 3085 a 308
+data_set2[marca2 == "PEUGEOT" & substr(MODELO_ITV2,1,4) =="3085", MODELO_ITV2:= "308"]
+
+#en CITROEN si modelo empieza por N la quitamos
+data_set2[marca2 == "CITROEN" & substr(modeloNoEsp,1,1) =="N" & substr(modeloNoEsp,1,4) !="NEMO", modeloNoEsp:= substr(modeloNoEsp,2,nchar(modeloNoEsp))]
+data_set2[marca2 == "CITROEN" & substr(modeloNoEsp,1,1) =="N" & substr(modeloNoEsp,1,4) !="NEMO", modelo1:= substr(modelo1,2,nchar(modelo1))]
+data_set2[marca2 == "CITROEN" & substr(modeloCorto,1,1) =="N"& substr(modeloNoEsp,1,4) !="NEMO", modeloCorto:= substr(modeloCorto,2,nchar(modeloCorto))]
+data_set2[marca2 == "CITROEN" & substr(MODELO_ITV2,1,1) =="N" & substr(modeloNoEsp,1,4) !="NEMO", MODELO_ITV2:= substr(MODELO_ITV2,2,nchar(MODELO_ITV2))]
+
+#en CITROEN C+' ' por c
+data_set2[marca2 == "CITROEN" & substr(modeloCorto,1,2) =="C ", modeloCorto:= paste("C", substr(modeloCorto,3,nchar(modeloCorto)) ,sep="")]
+
+#en CITROEN DS+' ' por DS
+data_set2[marca2 == "CITROEN" & substr(modeloCorto,1,3) =="DS ", modeloCorto:= paste("DS", substr(modeloCorto,4,nchar(modeloCorto)) ,sep="")]
+
+#en CITROEN BERL  por BELINGO
+data_set2[marca2 == "CITROEN" & substr(modeloCorto,1,4) =="BERL", modeloCorto:= "BERLINGO"]
+
+#en CITROEN lo que contega ELYS es C-ELYSSE
+data_set2[marca2 == "CITROEN" & grepl("ELYS",modeloCorto), modeloCorto:= "C-ELYSEE"]
+
+#en CITROEN lo que contega GC4 es GRAND C4
+data_set2[marca2 == "CITROEN" & modeloCorto == "GC4", pre:= "GRAN"]
+data_set2[marca2 == "CITROEN" & modeloCorto == "G4", pre:= "GRAN"]
+data_set2[marca2 == "CITROEN" & modeloCorto == "GC 4", pre:= "GRAN"]
+
+data_set2[marca2 == "CITROEN" & modeloCorto == "GC4", modeloCorto:= "C4"]
+data_set2[marca2 == "CITROEN" & modeloCorto == "G4", modeloCorto:= "C4"]
+data_set2[marca2 == "CITROEN" & modeloCorto == "GC 4", modeloCorto:= "C4"]
+
+# kk <- data_set2[marca2=="CITROEN" & modeloCorto == "CELYS BHDI100"]
+
+
+#EN FORD _MAX y ' MAX' a '-MAX'
+data_set2[marca2 == "FORD" & nchar(modeloCorto)<3 & suf == "MAX", modeloCorto:= paste(substr(modeloCorto,1,1),"-",sep="")]
+kk <- data_set2[marca2 == "FORD" & suf == "MAX" &  nchar(modeloCorto)<3]
+kk <- data_set2[marca2 == "FORD" & suf == "MAX" &  nchar(modeloCorto)>=3]
+#  en KIA CEE a CEED
+data_set2[marca2 == "KIA" & substr(modeloNoEsp,1,3) =="CEE", modeloCorto:= "CEED"]
+
+# en VOLKSWAGEN UP a UP!
+data_set2[marca2 == "VOLKSWAGEN" & substr(modeloNoEsp,1,2) =="UP", modeloCorto:= "UP!"]
+
+# En NISSAN E ' a 'E-'
+data_set2[marca2 == "NISSAN" & substr(MODELO_ITV2,1,2) =="E ", modeloCorto:= paste("E-", substr(modeloCorto,3,nchar(modeloCorto)) ,sep="")]
+data_set2[marca2 == "NISSAN" & substr(MODELO_ITV2,1,2) =="E ", modeloCorto:= paste("E_", substr(modeloCorto,3,nchar(modeloCorto)) ,sep="")]
+
+# En OPEL INSIGINIA a INSIGNIA
+data_set2[marca2 == "OPEL" & substr(modeloCorto,1,9) =="INSIGINIA", modeloCorto:= "INSIGNIA"]
+
+# LAND ROVER XE a JAGUAR XE	
+data_set2[marca2 == "LANDROVER" & substr(MODELO_ITV2,1,2)=="XE", marca1:= "JAGUAR"]
+data_set2[marca2 == "LANDROVER" & substr(MODELO_ITV2,1,2) =="XE", MARCA_ITV:= "JAGUAR"]
+data_set2[marca2 == "LANDROVER" & substr(MODELO_ITV2,1,2) =="XE", marca2:= "JAGUAR"]
+
+# GUILIETTA a GIULIETTA
+data_set2[marca2 == "ALFAROMEO" & modeloCorto =="GUILIETTA", modeloCorto:= "GIULIETTA"]
+
+
+# nos quedamos sólo del modelo hasta que acaba el primer numero
+
 setkey(data_set2,"codItv2")
-data_set2 <- data_set2[codItv2 != "        "]
 
 # asignar la marca más frecuente a cada codItv2
+data_set2 <- data_set2[codItv2 != "        "]
+data_set2_blancos <- data_set2[codItv2 == "        "]
 data_set2[,N:=.N, by = "codItv2,marca2"] 
 data_set2[,M:=max(N), by= "codItv2"] 
 tab <- data_set2[N == M, c("codItv2","MARCA_ITV","marca1","marca2"),with = F]
@@ -188,76 +330,85 @@ data_set2[,marca1 := NULL]
 data_set2[,marca2 := NULL]
 # data_set2[,MARCA_ITV := NULL]
 data_set2  <- merge(data_set2, tab, all.x = F,by.x = c("codItv2"), by.y = c("codItv2"))
-data_set2 <- rbind(data_set2,data_set[codItv2 == "        "])
+data_set2 <- rbind(data_set2,data_set2_blancos)
 
-#eliminamos del MODELO_ITV la marca
-data_set2[, MODELO_ITV :=  mapply(gsub, marca2, "", MODELO_ITV, fixed= T)]
-data_set2[, MODELO_ITV :=  mapply(gsub, marca1, "", MODELO_ITV, fixed= T)]
+#eliminamos del MODELO_ITV2 la marca  XXX creo que se puede elimina
+data_set2[, MODELO_ITV2 :=  mapply(gsub, marca2, "", MODELO_ITV2, fixed= T)]
+data_set2[, MODELO_ITV2 :=  mapply(gsub, marca1, "", MODELO_ITV2, fixed= T)]
 
 # 
 # kk <-data_set2[marca2.x!=marca2.y]
-# kk <- kk[,N:=.N,by ="codItv2,MODELO_ITV.x,modelo1.x,modelo2.x,MARCA_ITV.x,marca2.x,MODELO_ITV.y,modelo1.y,modelo2.y,MARCA_ITV.y,marca2.y"]
-# kk <- kk[,c("codItv2","MODELO_ITV.x","modelo1.x","modelo2.x","MARCA_ITV.x","marca2.x","MODELO_ITV.y","modelo1.y","modelo2.y","MARCA_ITV.y","marca2.y","N"),with =F]
+# kk <- kk[,N:=.N,by ="codItv2,MODELO_ITV2.x,modelo1.x,modeloNoEsp.x,MARCA_ITV.x,marca2.x,MODELO_ITV2.y,modelo1.y,modeloNoEsp.y,MARCA_ITV.y,marca2.y"]
+# kk <- kk[,c("codItv2","MODELO_ITV2.x","modelo1.x","modeloNoEsp.x","MARCA_ITV.x","marca2.x","MODELO_ITV2.y","modelo1.y","modeloNoEsp.y","MARCA_ITV.y","marca2.y","N"),with =F]
 # kk <- kk[N !=1,]
 # kk <- unique(kk)
 # setkey(kk,"N")
 # View(kk)       
 
 
-# asignar el 'MODELO_ITV' más frecuente a cada codItv2
+# asignar el 'MODELO_ITV2' más frecuente a cada codItv2
 # XXX habría que quitar los modelos en blanco por si son el más frecuente
 # data_set  <-  data_set[marca2 == "BMW"]
 data_set2_blancos <- data_set2[codItv2 == "        "]
 data_set2 <- data_set2[codItv2 != "        "]
-data_set2[,N:=.N, by = "codItv2,MODELO_ITV"] 
+data_set2[,N:=.N, by = "codItv2,MODELO_ITV2"] 
 data_set2[,M:=max(N), by= "codItv2"] 
-tab <- data_set2[N == M, c("MODELO_ITV","codItv2","modelo1","modelo2","MARCA_ITV","marca2"),with = F]
+tab <- data_set2[N == M, c("MODELO_ITV2","codItv2","modelo1","modeloNoEsp","MARCA_ITV","marca2"),with = F]
 setkeyv(tab,c("codItv2"))
 tab <- unique(tab)
 data_set2[,M := NULL]
 data_set2[,N := NULL]
-data_set2[,MODELO_ITV := NULL]
+data_set2[,MODELO_ITV2 := NULL]
 data_set2[,modelo1 := NULL]
-data_set2[,modelo2 := NULL]
+data_set2[,modeloNoEsp := NULL]
 data_set2[,MARCA_ITV := NULL]
 data_set2[,marca2 := NULL]
 # data_set2[,MARCA_ITV := NULL]
 data_set2  <- merge(data_set2, tab, all.x = F,by.x = c("codItv2"), by.y = c("codItv2"))
 data_set2 <- rbind(data_set2,data_set2_blancos)
 
-# kk <- data_set_def[data_set_def$MODELO_ITV.x != data_set_def$MODELO_ITV.y, c("codItv2","MODELO_ITV.x","MODELO_ITV.y", "MARCA_ITV.x","MARCA_ITV.y","marca2.x","marca2.y","modelo2.x","modelo2.y"),with =F]
+# kk <- data_set_def[data_set_def$MODELO_ITV2.x != data_set_def$MODELO_ITV2.y, c("codItv2","MODELO_ITV2.x","MODELO_ITV2.y", "MARCA_ITV.x","MARCA_ITV.y","marca2.x","marca2.y","modeloNoEsp.x","modeloNoEsp.y"),with =F]
 # View(kk)
 
-# asignar el 'MODELO_ITV' más frecuente a cada modelo2
+# asignar el 'MODELO_ITV2' más frecuente a cada modeloNoEsp
 # data_set2  <-  data_set[marca2 == "BMW"]
-data_set2_blancos  <-  data_set2[modelo2 == ""]
-data_set2  <-  data_set2[modelo2 != ""]
+data_set2_blancos  <-  data_set2[modeloNoEsp == ""]
+data_set2  <-  data_set2[modeloNoEsp != ""]
 setkey(data_set2,"marca2")
-data_set2[,N:=.N, by = "marca2,MODELO_ITV"] 
-data_set2[,M:=max(N), by= "marca2,modelo2"] 
-# tab <- data_set2[N == M, c("modelo2", "MODELO_ITV", "MARCA_ITV","marca2"),with = F]
-tab <- data_set2[N == M, c("modelo2", "MODELO_ITV", "marca2","modelo1"),with = F]
-setkeyv(tab,c("modelo2", "marca2"))
+data_set2[,N:=.N, by = "marca2,MODELO_ITV2"] 
+data_set2[,M:=max(N), by= "marca2,modeloNoEsp"] 
+# tab <- data_set2[N == M, c("modeloNoEsp", "MODELO_ITV2", "MARCA_ITV","marca2"),with = F]
+tab <- data_set2[N == M, c("modeloNoEsp", "MODELO_ITV2", "marca2","modelo1"),with = F]
+setkeyv(tab,c("modeloNoEsp", "marca2"))
 tab <- unique(tab)
 data_set2[,M := NULL]
 data_set2[,N := NULL]
-data_set2[,MODELO_ITV := NULL]
+data_set2[,MODELO_ITV2 := NULL]
 data_set2[,modelo1 := NULL]
-data_set2  <- merge(data_set2, tab, all.x = F,by.x = c("modelo2", "marca2"), by.y = c("modelo2", "marca2"))
+data_set2  <- merge(data_set2, tab, all.x = F,by.x = c("modeloNoEsp", "marca2"), by.y = c("modeloNoEsp", "marca2"))
 # kk <- head(data_set0, 100)
 data_set2 <- rbind(data_set2,data_set2_blancos)
 
 
-# seleccionamos las columnas que vamos a utilizar para los graficos
-# data_set[,.N, by= CLASIFICACION_REGLAMENTO_VEHICULOS_ITV][order(N, decreasing = T)]
-data_set2 <- data_set2[,col_sel, with=F]
+# Volvemos a poner los sufijos y prefijos
+data_set2[!is.na(pre), modeloCorto := paste(pre, modeloCorto)]
+data_set2[!is.na(suf), modeloCorto := paste(modeloCorto, suf)]
+data_set2[!is.na(suf), modeloCorto := gsub("\\s+-","-",modeloCorto)]
+data_set2[!is.na(suf), modeloCorto := gsub("-\\s+","-",modeloCorto)]
 
-#### filtraremos por marca/modelo
+#### Quitamos espacio delrpincipio y del final del municipio
 data_set2$MUNICIPIO <- gsub("^\\s+|\\s+$", "",as.character(data_set2$MUNICIPIO))
 data_set2$COD_PROVINCIA_VEH <- gsub("^\\s+|\\s+$", "",as.character(data_set2$COD_PROVINCIA_VEH))
 
 
-#### convertir en factores
+
+# seleccionamos las columnas que vamos a utilizar para los graficos
+# data_set[,.N, by= CLASIFICACION_REGLAMENTO_VEHICULOS_ITV][order(N, decreasing = T)]
 data_set_def <- data_set2
+data_set_def <- data_set_def[,col_sel, with=F]
+
+
+#### convertir en factores
+
 for (c in col_sel) {data_set2[[c]] <- as.factor(data_set2[[c]])}
 saveRDS(data_set2, file.path(DirDat, fichDatTur))
